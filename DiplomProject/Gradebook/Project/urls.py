8640f28a65e9d87788de05432d55113ac43b6dcf
NikitaAlekseyonok <nek.alekss@mail.ru>
2021-04-07 14:40:05 +0300

    second commiy

--- DiplomProject/Gradebook/Project/urls.py
+++ DiplomProject/Gradebook/Project/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('#', headTeacherHomePage, name='headTeacherHomePage'),
-    path('', studentPage, name='studentPage'),
+    path('', headTeacherHomePage, name='headTeacherPage'),
+    path('headTeacherPage', studentPage, name='studentPage'),
+    path('login', user_login, name='login'),
+    path('logout', user_logout, name='logout'),
+    path('logout', user_logout, name='logout'),
 ]
--- DiplomProject/Gradebook/Project/views.py
+++ DiplomProject/Gradebook/Project/views.py
@@ -6,12 +6,22 @@ from .forms import *
 
 
 def is_member(user):
-    return user.groups.filter(name='Member').exists()
+    if user.groups.filter(name='HeadTeacher').exists():
+        return 'HeadTeacher'
+    elif user.groups.filter(name='Teacher').exists():
+        return 'Teacher'
+    elif user.groups.filter(name='Student').exists():
+        return 'Student'
 
 
 def startPage(request):
     if request.user.is_authenticated:
-        return redirect('')
+        if is_member(request.user) == 'HeadTeacher':
+            return redirect('headTeacherPage')
+        if is_member(request.user) == 'Teacher':
+            return redirect('')
+        if is_member(request.user) == 'Student':
+            return redirect('')
     else:
         return redirect('login')
 
@@ -36,10 +46,16 @@ def user_login(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+
+            if is_member(request.user) == 'HeadTeacher':
+                return redirect('headTeacherPage')
+            if is_member(request.user) == 'Teacher':
+                return redirect('')
+            if is_member(request.user) == 'Student':
+                return redirect('')
     else:
         form = UserLoginForm()
-    return render(request, 'BD_Report/login.html', {'form': form})
+    return render(request, 'Project/login.html', {'form': form})
 
 
 def user_logout(request):
